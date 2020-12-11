fd5b58c7006a3de86cf9d16626551147d30ab232
Willian Sostenes <sostenes655@gmail.com>
2020-12-11 00:06:58 -0300

    Realizando testes com VAR DYNCAMIC E MAP

--- Curso_dart/fundamentos/tipos_básicos.dart
+++ Curso_dart/fundamentos/tipos_básicos.dart
@@ -1,16 +1,19 @@
 main() {
-  dynamic nomes = [
+  var nomes = [
     'Willian',
     'Helloisa',
     'Graciene',
   ];
   //print(nomes);
-  print(nomes[0]);
+  //print(nomes[0]);
+  //print(nomes[1]);
 
-  Map telefones = {
-    'Willian': '11 98821-6426',
-    'Helloisa': '11 99999-5667',
-    'Graciene': '11 99215-1243',
+  var telefones = {
+    'Willian': '11 98821-6426 ' + nomes[0].toString(),
+    'Helloisa': '11 99999-5667 ' + nomes[1].toString(),
+    'Graciene': '11 99215-1243 ' + nomes[2].toString(),
   };
   print(telefones['Willian']);
+  print(telefones['Helloisa']);
+  print(telefones['Graciene']);
 }
