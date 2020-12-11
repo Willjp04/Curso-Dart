fe5dfdbfdcc91e5f5cf9e52ce984bda491fed0f3
Willian Sostenes <sostenes655@gmail.com>
2020-12-11 19:22:18 -0300

    Utilizando operadores relacionais

--- Curso_dart/fundamentos/operadores_2.dart
+++ Curso_dart/fundamentos/operadores_2.dart
@@ -3,4 +3,9 @@ main() {
   dynamic a = 10;
   a /= 2;
   print(a);
+
+  // OPERADORES RELACIONAIS  - O RESULTADO SEMPRE É BOOL
+  print(4 > 3); // > MAIOR      // != DIFERENTE
+  print(10 != 9); // < MENOR      // <= MENOR OU IGUAL
+  print(4 == 3); // == IGUAL     // >= MAIOR OU IGUAL
 }
