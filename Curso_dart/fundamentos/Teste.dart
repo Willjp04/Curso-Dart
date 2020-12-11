b869543390b0f3ed4c2f81d44989780375e888e4
Willian Sostenes <sostenes655@gmail.com>
2020-12-11 19:42:04 -0300

    Realizando testes utilizando operadores e calculos

---
+++ Curso_dart/fundamentos/Teste.dart
@@ -0,0 +1,16 @@
+main() {
+  String a1 = "Nome do Aluno: Willian";
+  String a2 = "Nota final do Aluno";
+  String a7 = "Média Parcial";
+
+  double a3 = 4.5;
+  double a4 = 5.0;
+  double a5 = 7.0;
+  double a6 = 8.3;
+  double provafinal = 6;
+  double total = (a3 + a4 + a5 + a6 + provafinal);
+
+  print(a1);
+  print(a7 + total.toString());
+  print(a2 + provafinal.toString());
+}
