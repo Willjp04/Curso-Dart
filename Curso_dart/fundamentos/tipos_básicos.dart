b1706ecf639fc1451bffdef5313081f71f33beea
Willian Sostenes <sostenes655@gmail.com>
2020-12-10 23:59:24 -0300

    Utilizando o Map no dart

--- Curso_dart/fundamentos/tipos_básicos.dart
+++ Curso_dart/fundamentos/tipos_básicos.dart
@@ -4,6 +4,13 @@ main() {
     'Helloisa',
     'Graciene',
   ];
-  print(nomes);
-  print(nomes[2]);
+  //print(nomes);
+  print(nomes[0]);
+
+  Map telefones = {
+    'Willian': '11 98821-6426',
+    'Helloisa': '11 99999-5667',
+    'Graciene': '11 99215-1243',
+  };
+  print(telefones['Willian']);
 }
