ae310bbafdd35530917b3ac1f1d51389e31b36d0
Willian Sostenes <sostenes655@gmail.com>
2020-12-11 03:07:51 -0300

    Executando testes com pontuação no dart

--- Curso_dart/fundamentos/notacao_ponto.dart
+++ Curso_dart/fundamentos/notacao_ponto.dart
@@ -1,7 +1,6 @@
 main() {
   double nota = 6.99.roundToDouble();
-  //print('A nota do aluno é: '.toUpperCase() + nota.toString());
+  print('A nota do aluno é: '.toUpperCase() + nota.toString());
 
-  var s5 = 'Willian'.substring(0, 7).padRight(5, "!!");
-  print(s5);
+  print(nota);
 }
