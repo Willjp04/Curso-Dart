f15e0d93d10eed5154e8cec5e3b600f46b172639
Willian Sostenes <sostenes655@gmail.com>
2020-12-11 19:01:47 -0300

    Utilizando operadores aritmeticos e lógicos

---
+++ Curso_dart/fundamentos/operadores_1.dart
@@ -0,0 +1,23 @@
+main() {
+  int a = 7;
+  int b = 3;
+  int resultado = a + b;
+
+// operadore aritméticos
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+
+// operadores lógicos
+
+  bool fragil = true;
+  bool caro = true;
+
+  print(fragil && caro); // E - DOIS PRECISAM SER VERDADEIROS
+  print(fragil || caro); // OU - UM DOS DOIS PRECISA SER VERDADEIRO
+  print(fragil ^
+      caro); // OU EXCLUSIVO - UM SOMENTE VERDADEIRO (PARA SER EXCLUSIVO)
+  print(!fragil);
+}
