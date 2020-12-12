f3919845091f2db86a2452d591c3e3efc470cc17
Willian Sostenes <sostenes655@gmail.com>
2020-12-12 14:18:54 -0300

    Utiizando o generics no dart

---
+++ Curso_dart/fundamentos/generics.dart
@@ -0,0 +1,12 @@
+main() {
+  List<String> frutas = ['banana', 'maçã', 'laranja'];
+  frutas.add('limão');
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 7500.00,
+    'vendedor': 2000.00,
+    'estagiário': 900.00,
+  };
+  print(salarios);
+}
